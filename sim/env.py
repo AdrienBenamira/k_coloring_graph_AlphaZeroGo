baded878e5ba68fcc28118f3a3360b54823242c2
Benamira Adrien <adrien.benamira@supelec.fr>
2019-06-12 23:03:26 +0200

    create graph and color matrix

--- sim/env.py
+++ sim/env.py
@@ -10,16 +10,22 @@ class Env:
         self.vertex_min = config.general.vertex_min
         self.seed = config.general.seed
         self.directed = config.general.directed
+        self.Z = config.general.number_color_init
         np.random.seed(self.seed)
 
     def _get_graph(self):
-        self.vertex = np.random.randint(self.vertex_min, self.vertex_max, ) + 1
+        self.vertex = np.random.randint(self.vertex_min, self.vertex_max)
         self.edges_proba = np.random.rand()
         self.G = nx.gnp_random_graph(self.vertex, self.edges_proba, seed=self.seed, directed=self.directed)
-
+        self.C = np.zeros(self.vertex, self.Z)
 
     def step(self, action):
-        pass
+        """
+
+        :param action: dimension 2 dim1 : le noeud a change dim2: la couleur
+        :return: C
+        """
+        self.C[action[0]][action[1]] = 1
 
     def reset(self):
         """
